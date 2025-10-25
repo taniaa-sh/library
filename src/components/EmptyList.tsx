1994efa519f41d933b8171e1190c8f362baecd11
tania shafiee <taniashafiee78@gmail.com>
2025-10-25 14:36:53 +0330

    fix empty list ui bug

--- src/components/EmptyList.tsx
+++ src/components/EmptyList.tsx
@@ -11,16 +11,16 @@ const Lottie = dynamic(() => import("lottie-react"), {
 
 const EmptyList = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center py-10 !mt-20">
+    <div className="flex flex-col items-center justify-center text-center !py-10 !mt-20">
       <Lottie
         animationData={animationData}
         loop
         className="w-[250px] sm:w-[400px] md:w-[500px]"
       />
-      <p className="mt-4 text-gray-600 dark:text-gray-300 text-lg sm:text-xl font-medium">
+      <p className="!mt-4 text-gray-600 dark:text-gray-300 text-lg sm:text-xl font-medium">
         No Data Found
       </p>
-      <p className="text-sm text-gray-500 dark:text-gray-400 mt-1">
+      <p className="text-sm text-gray-500 dark:text-gray-400 !mt-1">
         Try adjusting your filters or adding new items.
       </p>
     </div>
