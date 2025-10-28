e9c322180e0e65f9b730a17504ba580a86ef21f7
tania shafiee <taniashafiee78@gmail.com>
2025-10-28 10:38:29 +0330

    fix ui

---
+++ src/app/admin/components/AdminHeader.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+const AdminHeader = () => {
+    return (
+        <div className="flex flex-col items-center justify-center text-center !py-10 !mt-20">
+        </div>
+    );
+};
+
+export default AdminHeader;
