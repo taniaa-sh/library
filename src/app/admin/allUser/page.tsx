d16d584f2c911d14297541a46d0d6427f1a9c89a
tania shafiee <taniashafiee78@gmail.com>
2025-10-28 12:22:49 +0330

    add all user table

--- src/app/admin/allUser/page.tsx
+++ src/app/admin/allUser/page.tsx
@@ -1,13 +1,52 @@
-"use client"
+import UserTableClient from "./_components/UserTableClient";
 
-const AllUsers = () => {
+const fetchUsers = async () => {
+    await new Promise((r) => setTimeout(r, 500));
+    return [
+        {
+            name: 'Tania',
+            dateJoined: 'Dec 19 2023',
+            role: 'Admin',
+            booksBorrowed: '3',
+            universityIDNumber: '123456789',
+            universityIDCard: '123456789',
+            action: 'Delete',
+            avatar: '/avatar1.png',
+        },
+        {
+            name: 'Ali',
+            dateJoined: 'Dec 20 2023',
+            role: 'User',
+            booksBorrowed: '1',
+            universityIDNumber: '987654321',
+            universityIDCard: '987654321',
+            action: 'Delete',
+            avatar: '/avatar2.png',
+        },
+        {
+            name: 'Sara',
+            dateJoined: 'Jan 05 2024',
+            role: 'User',
+            booksBorrowed: '0',
+            universityIDNumber: '112233445',
+            universityIDCard: '112233445',
+            action: 'Delete',
+            avatar: '/avatar3.png',
+        },
+    ];
+};
+
+const AllUsersPage = async () => {
+    const data = await fetchUsers();
+
     return (
-        <div className="!bg-[#F8F8FF] !px-6">
-            <div className="flex flex-col gap-6 bg-white !py-[23px] !px-5">
+        <div className="bg-[#F8F8FF] px-6 py-6 min-h-screen">
+            <div className="flex flex-col gap-6 bg-white py-6 px-5 rounded-lg">
                 <p className="font-medium text-xl">All Users</p>
+                <UserTableClient data={data} />
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default AllUsers
+export default AllUsersPage;
