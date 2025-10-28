8ade8c0c9b2ba7cce4e47ce1c0e1fcd04c8b02f2
tania shafiee <taniashafiee78@gmail.com>
2025-10-28 10:43:27 +0330

    add admin header

--- src/app/admin/components/AdminHeader.tsx
+++ src/app/admin/components/AdminHeader.tsx
@@ -4,7 +4,11 @@ import React from "react";
 
 const AdminHeader = () => {
     return (
-        <div className="flex flex-col items-center justify-center text-center !py-10 !mt-20">
+        <div className="flex items-center justify-between w-full !p-6">
+            <div className="flex flex-col gap-[6px]">
+                <p className="font-semibold text-2xl">Welcome, Adrian</p>
+                <p className="font-normal text-base text-slate-500">Monitor all of your projects and tasks here</p>
+            </div>
         </div>
     );
 };
