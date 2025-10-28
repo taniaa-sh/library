ba1f0fecd256eb7c7ee3b7c8d99022c55c0f8312
tania shafiee <taniashafiee78@gmail.com>
2025-10-28 14:23:04 +0330

    add custom status component

---
+++ src/app/admin/components/CustomStatus.tsx
@@ -0,0 +1,85 @@
+import Image from "next/image";
+
+type PropsType = {
+    text: string;
+    size: "medium" | "small";
+    color: "blue" | "red" | "yellow" | "lightYellow" | "lightGreen" | "green" | "gray" | "purple" | "orange" | "darkBlue" | "pink";
+    width?: string;
+    containerClassName?: string;
+    icon?: string
+    iconClass?: string
+    iconClick?: () => void
+};
+
+const CustomStatus: React.FC<PropsType> = ({
+    text,
+    size,
+    color,
+    width,
+    containerClassName,
+    icon,
+    iconClass,
+    iconClick
+}) => {
+    let newClass1 = "";
+    let newClass2 = "";
+    if (color === "blue") {
+        newClass1 = "text-brand600 bg-brand50 dark:bg-[#1d2939] dark:text-[#2e90fa]";
+        newClass2 = "bg-brand600 dark:bg-[#2e90fa]";
+    } else if (color === "red") {
+        newClass1 = "text-error600 bg-error50 dark:bg-[#4a161a] dark:text-[#f97066]";
+        newClass2 = "bg-error600 dark:bg-[#f97066]";
+    } else if (color === "yellow") {
+        newClass1 = "text-warning600 bg-warning50 dark:bg-[#43320f] dark:text-[#dc6803]";
+        newClass2 = "bg-warning600 dark:bg-[#dc6803]";
+    } else if (color === "green") {
+        newClass1 = "text-success600 bg-success50 dark:bg-[#0e312a] dark:text-[#32d583]";
+        newClass2 = "bg-success600 dark:bg-[#32d583]";
+    } else if (color === "gray") {
+        newClass1 = "text-gray600 bg-gray50 dark:bg-[#1d2939] dark:text-[#eaecf0]";
+        newClass2 = "bg-gray600 dark:bg-[#eaecf0]";
+    } else if (color === "purple") {
+        newClass1 = "text-purple600 bg-purple50 dark:bg-[#eceffd] dark:text-[#300398]";
+        newClass2 = "bg-purple600 dark:bg-[#300398]";
+    } else if (color === "orange") {
+        newClass1 = "text-orange600 bg-orange50 dark:bg-[#fff4eb] dark:text-[#dc5d03]";
+        newClass2 = "bg-orange600 dark:bg-[#dc5d03]";
+    } else if (color === "lightYellow") {
+        newClass1 = "text-lightYellow600 bg-lightYellow50 dark:bg-[#43320f] dark:text-[#fec84b]";
+        newClass2 = "bg-lightYellow600 dark:bg-[#fec84b]";
+    } else if (color === "darkBlue") {
+        newClass1 = "text-blue600 bg-blue50";
+        newClass2 = "bg-blue600";
+    } else if (color === "pink") {
+        newClass1 = "!text-[#C11574] !bg-[#FDF2FA]";
+        newClass2 = "bg-[#C11574]";
+    } else if (color === "lightGreen") {
+        newClass1 = "!text-[#027A48] !bg-[#ECFDF3]";
+        newClass2 = "bg-[#027A48]";
+    }
+    return (
+        <div
+            className={`whitespace-nowrap gap-[6px] flex items-start rounded-[16px] py-1 ${size === "medium" ? "px-4" : size === "small" ? "px-3" : ""
+                } ${newClass1} ${containerClassName !== undefined ? containerClassName : ""}`}
+            style={{ width: width !== undefined ? width : "" }}
+        >
+            {
+                icon ? (
+                    <Image
+                        src={icon}
+                        alt="icon"
+                        width={12}
+                        height={12}
+                        className={`${iconClass} self-center`}
+                        onClick={() => iconClick && iconClick()}
+                    />
+                ) : (
+                    <div className={`w-[6px] h-[6px] mt-2 rounded ${newClass2}`} />
+                )
+            }
+            <p className="whitespace-nowrap leading-6 text-xs font-medium">{text}</p>
+        </div>
+    );
+};
+
+export default CustomStatus;
