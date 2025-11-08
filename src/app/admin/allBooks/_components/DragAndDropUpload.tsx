76b7dfa1d6ede157f7a46cf0e26bed135a3cc0d2
tania shafiee <taniashafiee78@gmail.com>
2025-11-08 10:15:51 +0330

    fix dragger in add new book

--- src/app/admin/allBooks/_components/DragAndDropUpload.tsx
+++ src/app/admin/allBooks/_components/DragAndDropUpload.tsx
@@ -12,6 +12,7 @@ type DragAndDropUploadProps = {
 
 const DragAndDropUpload = ({ onChange, preview }: DragAndDropUploadProps) => {
     const [imagePreview, setImagePreview] = useState<string | null>(preview || null);
+    const [fileName, setFileName] = useState<string | null>(null);
     const [isDragging, setIsDragging] = useState(false);
     const inputRef = useRef<HTMLInputElement | null>(null);
 
@@ -44,55 +45,69 @@ const DragAndDropUpload = ({ onChange, preview }: DragAndDropUploadProps) => {
         }
         const url = URL.createObjectURL(file);
         setImagePreview(url);
+        setFileName(file.name);
         onChange(file);
     };
 
     return (
-        <div className={`w-full h-44 !bg-[#F9FAFB] border-2 border-dashed rounded-xl flex flex-col items-center justify-center cursor-pointer transition-all ${isDragging ? 'border-blue-500 bg-blue-50' : "border-gray-300 hover:border-blue-400"
-            }`}
-            onClick={() => inputRef.current?.click()}
-            onDragOver={handleDragOver}
-            onDragLeave={handleDragLeave}
-            onDrop={handleDrop}
-        >
-            <div className="flex flex-col gap-6 items-center text-center text-gray-500">
-                <div className="flex items-center gap-2">
-                    <Image
-                        src={imagesAddresses.icons.upload}
-                        alt="upload"
-                        width={18}
-                        height={18}
+        <>
+            <div className={`w-full h-44 !bg-[#F9FAFB] border-2 border-dashed rounded-xl flex flex-col items-center justify-center cursor-pointer transition-all ${isDragging ? 'border-blue-500 bg-blue-50' : "border-gray-300 hover:border-blue-400"
+                }`}
+                onClick={() => inputRef.current?.click()}
+                onDragOver={handleDragOver}
+                onDragLeave={handleDragLeave}
+                onDrop={handleDrop}
+            >
+                <div className="flex flex-col gap-6 items-center text-center text-gray-500">
+                    <div className="flex items-center gap-2">
+                        <Image
+                            src={imagesAddresses.icons.upload}
+                            alt="upload"
+                            width={18}
+                            height={18}
+                        />
+                        <p className="font-normal text-base leading 6 text-slate-500">Upload an image</p>
+                    </div>
+                    <AdminButton
+                        text="Browse to Upload"
+                        color="blue"
+                        containerClassName="cursor-pointer"
                     />
-                    <p className="font-normal text-base leading 6 text-slate-500">Upload an image</p>
                 </div>
-                <AdminButton
-                    text="Browse to Upload"
-                    color="blue"
-                    containerClassName="cursor-pointer"
+
+                <input
+                    type="file"
+                    accept="image/*"
+                    ref={inputRef}
+                    onChange={handleFileChange}
+                    className="hidden"
                 />
-            </div>
+            </div >
             {
                 imagePreview && (
-         <div className="w-full !bg-[#F9FAFB] border-2 border-gray-300 rounded-xl flex justify-between items-center !mt-20">
-                       <Image
-                        src={imagePreview}
-                        alt="preview"
-                        width={20}
-                        height={20}
-                        className="object-cover rounded-lg shadowr"
-                    />
-                    cfbc
-        </div>
+                    <div className="w-full !bg-[#F9FAFB] border-2 border-[#7286dd] rounded-xl flex justify-between items-center !mt-4 !p-4">
+                        <div className="flex gap-2 items-center">
+                            <Image
+                                src={imagePreview}
+                                alt="preview"
+                                width={40}
+                                height={40}
+                                className="object-cover rounded-lg shadowr"
+                            />
+                            <p className="text-sm font-medium text-gray-700">{fileName}</p>
+                        </div>
+                        <Image
+                            src={imagesAddresses.icons.delete}
+                            alt="close"
+                            width={20}
+                            height={20}
+                            className="cursor-pointer"
+                            onClick={() => setImagePreview(null)}
+                        />
+                    </div>
                 )
             }
-            <input
-                type="file"
-                accept="image/*"
-                ref={inputRef}
-                onChange={handleFileChange}
-                className="hidden"
-            />
-        </div >
+        </>
     )
 }
 
