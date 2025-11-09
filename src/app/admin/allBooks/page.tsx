5f056ad36b943b0a5858e8718e0a79ce23dede73
tania shafiee <taniashafiee78@gmail.com>
2025-11-09 11:13:32 +0330

    fix bugs

--- src/app/admin/allBooks/page.tsx
+++ src/app/admin/allBooks/page.tsx
@@ -1,31 +1,48 @@
 import AllBooksTableClient from "./_components/AllBooksTableClient";
 import AddBookBtn from "./_components/AddBookBtn";
 
-async function fetchBooks() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
-  const res = await fetch(`${baseUrl}/api/books`, { cache: "no-store" });
+const fetchUsers = async () => {
 
-  if (!res.ok) {
-    throw new Error("Failed to fetch books");
-  }
-
-  return res.json();
-}
+    await new Promise((r) => setTimeout(r, 500));
+    return [
+        {
+            bookTitle: 'The Great Reclamation: A Novel by',
+            author: 'Rachel Hxeng',
+            genre: 'Strategic, Fantasy',
+            dateCreated: 'Dec 19 2023',
+            action: '',
+        },
+        {
+            bookTitle: 'Inside Evil: Inside Evil Series, Book 1',
+            author: 'Rachel Hxeng',
+            genre: 'Strategic, Fantasy',
+            dateCreated: '1',
+            action: '',
+        },
+        {
+            bookTitle: 'Jayne Castle - People in Glass Houses',
+            author: 'Rachel Hxeng',
+            genre: 'Strategic, Fantasy',
+            dateCreated: 'Dec 19 2023',
+            action: '',
+        },
+    ];
+};
 
 const AllBooksPage = async () => {
-  const data = await fetchBooks();
+    const data = await fetchUsers();
 
-  return (
-    <div className="bg-[#F8F8FF] px-6 py-6">
-      <div className="flex flex-col gap-6 bg-white py-6 px-5 rounded-lg">
-        <div className="flex justify-between items-center">
-          <p className="font-medium text-xl">All Books</p>
-          <AddBookBtn />
+    return (
+        <div className="bg-[#F8F8FF] px-6 py-6">
+            <div className="flex flex-col gap-6 bg-white py-6 px-5 rounded-lg">
+                <div className="flex justify-between items-center">
+                    <p className="font-medium text-xl">All Books</p>
+                    <AddBookBtn />
+                </div>
+                <AllBooksTableClient data={data} />
+            </div>
         </div>
-        <AllBooksTableClient data={data} />
-      </div>
-    </div>
-  );
+    );
 };
 
 export default AllBooksPage;
