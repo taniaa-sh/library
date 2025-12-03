752ba4356d9c77d6fec766b50d69c257f830e800
tania shafiee <taniashafiee78@gmail.com>
2025-12-03 11:46:10 +0330

    add darkMode for sign-up

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -6,7 +6,7 @@ import imagesAddresses from '@/utils/imageAddresses'
 import SiteUrls from '@/utils/routs'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { toast } from 'sonner'
 
 const SignUp = () => {
@@ -19,6 +19,15 @@ const SignUp = () => {
   const [isLoading, setIsLoading] = useState(false)
   const [showUniversityId, setShowUniversityId] = useState(false)
   const [showPass, setShowPass] = useState(false)
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, []);
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -53,30 +62,37 @@ const SignUp = () => {
   return (
     <div className="w-full flex items-center flex-col lg:flex-row overflow-y-hidden">
       <div className="w-full h-screen bg-[url('/images/loginBg.png')] bg-cover bg-center p-6 md:p-10 lg:p-20 flex items-center justify-center">
-        <div className="bg-gray-900 w-full max-w-md md:max-w-lg lg:max-w-none p-6 md:p-8 lg:p-10 flex flex-col gap-8 rounded-lg">
+        <div className="bg-gray-900 dark:bg-gray-50 w-full max-w-md md:max-w-lg lg:max-w-none p-6 lg:p-10 flex flex-col gap-8 rounded-lg">
           <div className="flex flex-col justify-start items-start gap-1">
             <Image
               src={imagesAddresses.images.logo}
               alt="logo"
               width={120}
               height={120}
-              className="lg:mx-0"
+              className="lg:mx-0 dark:hidden"
             />
-            <h1 className="text-xl md:text-2xl font-bold text-white lg:text-left !mt-4">
+            <Image
+              src={imagesAddresses.icons.FrameWhite}
+              alt="logo"
+              width={120}
+              height={120}
+              className="lg:mx-0 hidden dark:block"
+            />
+            <h1 className="text-xl md:text-2xl font-bold text-white dark:text-black lg:text-left !mt-2">
               Create Your Library Account
             </h1>
-            <p className="text-gray-400 text-sm md:text-base font-normal lg:text-left -mt-2">
+            <p className="text-gray-400 dark:text-gray-600 text-sm md:text-base font-normal lg:text-left -mt-2">
               Please complete all fields and upload a valid university ID to gain access to the library
             </p>
           </div>
 
-          <form className="w-full flex flex-col gap-3 md:gap-4 lg:gap-6 text-white">
+          <form className="w-full flex flex-col gap-3 md:gap-4 lg:gap-6 text-white dark:text-gray-900">
             {/* Full Name */}
             <div className="flex flex-col gap-1">
               <label htmlFor="fullname" className="text-sm md:text-base lg:text-lg">Full Name</label>
               <input
                 id="fullname"
-                className="w-full bg-[#232839] p-2 md:p-3 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
+                className="w-full bg-[#232839] dark:bg-gray-50 dark:border dark:border-gray-300 p-2 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
                 type="text"
                 placeholder="Enter your full name"
                 onChange={(e) => setFirstName(e.target.value)}
@@ -88,7 +104,7 @@ const SignUp = () => {
               <label htmlFor="email" className="text-sm md:text-base lg:text-lg">Email</label>
               <input
                 id="email"
-                className="w-full bg-[#232839] p-2 md:p-3 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
+                className="w-full bg-[#232839] dark:bg-gray-50 dark:border dark:border-gray-300 p-2 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
                 type="email"
                 placeholder="Enter your email"
                 onChange={(e) => setEmail(e.target.value)}
@@ -101,7 +117,7 @@ const SignUp = () => {
               <input
                 id="universityId"
                 maxLength={11}
-                className="w-full bg-[#232839] p-2 md:p-3 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
+                className="w-full bg-[#232839] dark:bg-gray-50 dark:border dark:border-gray-300 p-2 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
                 type={showUniversityId ? "text" : "password"}
                 placeholder="Enter your university ID number"
                 onChange={(e) => setUniversityIDNumber(e.target.value)}
@@ -121,7 +137,7 @@ const SignUp = () => {
               <label htmlFor="password" className="text-sm md:text-base lg:text-lg">Password</label>
               <input
                 id="password"
-                className="w-full bg-[#232839] p-2 md:p-3 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
+                className="w-full bg-[#232839] dark:bg-gray-50 dark:border dark:border-gray-300 p-2 rounded-lg placeholder-gray-400 text-sm md:text-base lg:text-lg"
                 type={showPass ? "text" : "password"}
                 maxLength={8}
                 placeholder="At least 8 characters long"
@@ -160,10 +176,10 @@ const SignUp = () => {
           </Button>
 
           {/* Login Redirect */}
-          <div className="text-white text-xs md:text-sm lg:text-base font-normal self-center">
+          <div className="text-white dark:text-gray-900 text-xs md:text-sm lg:text-base font-normal self-center">
             Have an account already?{" "}
             <span
-              className="text-light-200 text-sm md:text-base cursor-pointer"
+              className="text-light-200 dark:text-[#8c6c2f] text-sm md:text-base cursor-pointer"
               onClick={() => router.push(SiteUrls.signIn)}
             >
               Login
@@ -172,7 +188,7 @@ const SignUp = () => {
         </div>
       </div>
 
-      <div className="hidden lg:block relative w-full h-[960px]">
+      <div className="hidden lg:block relative w-full h-screen">
         <Image
           src={imagesAddresses.images.loginPic}
           alt="logo"
