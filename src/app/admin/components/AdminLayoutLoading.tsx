8f76311ce52afd15948050e357023bf1d3ce17f7
tania shafiee <taniashafiee78@gmail.com>
2025-12-03 14:04:35 +0330

    fix loading

--- src/app/admin/components/AdminLayoutLoading.tsx
+++ src/app/admin/components/AdminLayoutLoading.tsx
@@ -1,26 +1,40 @@
-"use client"
+"use client";
 
-import React from 'react'
-import animationData from "../../../../public/lottie/supportLoading.json"
-import dynamic from 'next/dynamic';
+import React, { useEffect, useState } from "react";
+import animationData from "../../../../public/lottie/supportLoading.json";
+import dynamic from "next/dynamic";
+import fallbackGif from "../../../../public/gif/LoadingAnimationBlue.gif";
 
-const Lottie = dynamic(() => import('lottie-react'), {
+const Lottie = dynamic(() => import("lottie-react"), {
     ssr: false,
     loading: () => null,
 });
 
 const AdminLayoutLoading = () => {
-    return (
-        <div className="fixed inset-0 size-full flex flex-col items-center justify-center md:left-36">
-            <Lottie
-                animationData={animationData}
-                loop={true}
-                reversed
-                className={"size-[100px] sm:size-[200px]"}
-            />
-            <p className='sm:!ml-12 -mt-6 text-sm md:text-lg'>Loading...</p>
-        </div>
-    )
-}
+    const [isClient, setIsClient] = useState(false);
 
-export default AdminLayoutLoading
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
+    return (
+        <div className="fixed inset-0 w-full h-full flex flex-col items-center justify-center md:left-36">
+            {isClient ? (
+                <Lottie
+                    animationData={animationData}
+                    loop={true}
+                    className="w-[100px] sm:w-[200px]"
+                />
+            ) : (
+                <img
+                    src={fallbackGif.src}
+                    alt="Loading..."
+                    className="w-[100px] sm:w-[200px]"
+                />
+            )}
+            <p className={`sm:!ml-12  text-sm md:text-lg`}>Loading...</p>
+        </div>
+    );
+};
+
+export default AdminLayoutLoading;
--- src/components/LayoutLoading.tsx
+++ src/components/LayoutLoading.tsx
@@ -1,25 +1,43 @@
-"use client"
+"use client";
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import animationData from "../../public/lottie/loading.json";
 import dynamic from 'next/dynamic';
+import fallbackGif from "../../public/gif/loading.gif";
 
-const Lottie = dynamic(() => import('lottie-react'), {
+const Lottie = dynamic(() => import("lottie-react"), {
     ssr: false,
     loading: () => null,
 });
 
-const LayoutLoading = () => {
+const AdminLayoutLoading = () => {
+    const [isClient, setIsClient] = useState(false);
+
+    useEffect(() => {
+        setIsClient(true);
+    }, []);
+
     return (
         <div className="fixed inset-0 size-full bg-gray-500/50 flex flex-col items-center justify-center gap-4">
-            <Lottie
-                animationData={animationData}
-                loop={true}
-                reversed
-                className={"size-[400px] sm:size-[600px]"}
-            />
-        </div>
-    )
-}
+            {isClient ? (
+                <Lottie
+                    animationData={animationData}
+                    loop={true}
+                    reversed
+                    className={"size-[400px] sm:size-[600px]"}
+                />
+            ) : (
+                <div className='flex flex-col gap-2'>
+                    <img
+                        src={fallbackGif.src}
+                        alt="Loading..."
+                        className="w-[400px] sm:w-[600px]"
+                    />
+                </div>
+            )}
 
-export default LayoutLoading
+        </div>
+    );
+};
+
+export default AdminLayoutLoading;
