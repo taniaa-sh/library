6d7d5f1f22a0d78a625943bba5d9f4b78e369f01
tania shafiee <taniashafiee78@gmail.com>
2025-12-03 11:44:17 +0330

    add darkMode for forgetPass

--- src/app/(auth)/forget-pass/page.tsx
+++ src/app/(auth)/forget-pass/page.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { toast } from "sonner"
 import Image from "next/image"
@@ -13,6 +13,15 @@ const ForgotPasswordPage = () => {
     const [email, setEmail] = useState("")
     const [loading, setLoading] = useState(false)
     const router = useRouter()
+
+    useEffect(() => {
+        const savedTheme = localStorage.getItem("theme");
+        if (savedTheme === "dark") {
+            document.documentElement.classList.add("dark");
+        } else {
+            document.documentElement.classList.remove("dark");
+        }
+    }, []);
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault()
@@ -40,7 +49,7 @@ const ForgotPasswordPage = () => {
         <div className="w-full flex items-center flex-col lg:flex-row">
             <div className="w-full h-screen bg-[url('/images/loginBg.png')] bg-cover bg-center p-6 md:p-10 lg:p-20 flex items-center justify-center">
                 <div className=" flex items-center justify-center">
-                    <div className="bg-[#1a1f2c] p-8 rounded-xl w-full max-w-md text-white flex flex-col gap-4">
+                    <div className="bg-[#1a1f2c] dark:bg-gray-50 p-8 rounded-xl w-full max-w-md text-white dark:text-black flex flex-col gap-4">
                         <Image
                             src={imagesAddresses.icons.arrowRightYellow2}
                             alt="logo"
@@ -54,7 +63,14 @@ const ForgotPasswordPage = () => {
                             alt="logo"
                             width={120}
                             height={120}
-                            className="lg:mx-0"
+                            className="lg:mx-0 dark:hidden"
+                        />
+                        <Image
+                            src={imagesAddresses.icons.FrameWhite}
+                            alt="logo"
+                            width={120}
+                            height={120}
+                            className="lg:mx-0 hidden dark:block"
                         />
                         <h1 className="text-2xl font-bold text-start !mt-2">Forgot Password</h1>
                         <p className="text-gray-400 text-start text-sm !-mt-2">
@@ -66,18 +82,18 @@ const ForgotPasswordPage = () => {
                                 <input
                                     id="email"
                                     type="email"
-                                    className="bg-[#232839] p-2 md:p-3 rounded-lg text-white text-sm md:text-base lg:text-lg"
+                                    className="bg-[#232839] dark:bg-gray-50 dark:border dark:border-gray-300 p-2 md:p-3 rounded-lg text-white dark:text-black text-sm md:text-base lg:text-lg"
                                     placeholder="your@email.com"
                                     onChange={(e) => setEmail(e.target.value)}
                                 />
                             </div>
 
                             <Button
-                                className="w-full cursor-pointer text-sm md:text-base lg:text-lg !text-black"
+                                className="w-full cursor-pointer text-sm md:text-base lg:text-lg !text-black dark:!text-white"
                                 type="submit"
                             >
                                 {loading ? (
-                                    <span className='w-4 h-4 rounded-full border-1 border-t-0 border-black animate-spin' />
+                                    <span className='w-4 h-4 rounded-full border-1 border-t-0 border-black dark:border-white animate-spin' />
                                 ) : (
                                     "Send Reset Link"
                                 )}
@@ -88,7 +104,7 @@ const ForgotPasswordPage = () => {
                 </div>
             </div>
 
-            <div className="hidden lg:block relative w-full h-[960px]">
+            <div className="hidden lg:block relative w-full h-screen">
                 <Image
                     src={imagesAddresses.images.loginPic}
                     alt="logo"
