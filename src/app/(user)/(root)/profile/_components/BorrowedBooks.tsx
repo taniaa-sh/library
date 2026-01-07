cc7e41cce90d413df0dfc9fe81979592077df968
tania shafiee <taniashafiee78@gmail.com>
2026-01-07 12:13:31 +0330

    add SwiperSlide in BorrowedBooks instead scroll

--- src/app/(user)/(root)/profile/_components/BorrowedBooks.tsx
+++ src/app/(user)/(root)/profile/_components/BorrowedBooks.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
 import CustomButton from "@/components/CustomButton";
 import imagesAddresses from "@/utils/imageAddresses";
 import Image from "next/image";
@@ -9,77 +13,93 @@ const books = [1, 2, 3, 4, 5, 6, 7, 8];
 const INITIAL_COUNT = 4;
 
 const BorrowedBooks = () => {
-
     const [visibleCount, setVisibleCount] = useState(INITIAL_COUNT);
 
     return (
         <div className="w-full mt-4 flex flex-col gap-4 md:gap-5">
-            <p className="text-light-100 dark:text-gray-900 font-semibold text-2xl md:text-3xl">Borrowed books</p>
-            <div className="hidden sm:flex sm:overflow-x-auto sm:gap-4 md:gap-5 py-2 custom-scrollbar1">
-                {books.map((item) => (
-                    <div
-                        key={item}
-                        className="flex-shrink-0 w-64 p-5 flex flex-col gap-5 bg-[url('/images/loginBg.png')] bg-gray-900 bg-cover dark:bg-gray-50 rounded-lg"
-                    >
-                        <div className="bg-gold500/60 rounded-lg px-12 py-6 flex justify-center">
-                            <Image
-                                src={imagesAddresses.images.book1}
-                                alt="book"
-                                width={120}
-                                height={120}
-                                className="-ml-4"
-                            />
-                        </div>
-                        <div className="flex flex-col gap-1">
-                            <p className="text-white dark:text-gray-900 font-semibold text-xl leading-6">
-                                The Origin
-                            </p>
-                            <p className="text-white dark:text-gray-900 font-semibold text-xl leading-6">
-                                By Dan Brown
-                            </p>
-                        </div>
-                        <p className="text-light-100 dark:text-gray-900 font-normal text-xl leading-5">
-                            Thriller / Mystery
-                        </p>
-                        <div className="flex gap-1">
-                            <Image
-                                src={imagesAddresses.icons.greenBook}
-                                alt="star"
-                                width={16}
-                                height={16}
-                            />
-                            <p className="text-light-100 dark:text-gray-900 font-normal text-base">
-                                Borrowed on Dec 31
-                            </p>
-                        </div>
-                        <div className="flex justify-between items-center">
-                            <div className="flex gap-1">
-                                <Image
-                                    src={imagesAddresses.icons.calender}
-                                    alt="calendar"
-                                    width={16}
-                                    height={16}
-                                />
-                                <p className="text-light-100 dark:text-gray-900 font-normal text-base">
-                                    04 days left to due
+            <p className="text-light-100 dark:text-gray-900 font-semibold text-2xl md:text-3xl">
+                Borrowed books
+            </p>
+
+            <div className="hidden sm:flex">
+                <Swiper
+                    modules={[Navigation]}
+                    spaceBetween={20}
+                    navigation={{
+                        nextEl: ".custom-next",
+                        prevEl: ".custom-prev",
+                    }}
+                    breakpoints={{
+                        500: { slidesPerView: 2 },
+                        768: { slidesPerView: 3 },
+                        1024: { slidesPerView: 4 },
+                        1280: { slidesPerView: 5 },
+                    }}
+                >
+                    {books.map((item) => (
+                        <SwiperSlide key={item}>
+                            <div className="flex flex-col gap-5 p-5 bg-[url('/images/loginBg.png')] bg-gray-900 bg-cover dark:bg-gray-50 rounded-lg cursor-grab">
+                                <div className="bg-gold500/60 rounded-lg px-12 py-6 flex justify-center">
+                                    <Image
+                                        src={imagesAddresses.images.book1}
+                                        alt="book"
+                                        width={120}
+                                        height={120}
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-1">
+                                    <p className="text-white dark:text-gray-900 font-semibold text-xl leading-6">
+                                        The Origin
+                                    </p>
+                                    <p className="text-white dark:text-gray-900 font-semibold text-xl leading-6">
+                                        By Dan Brown
+                                    </p>
+                                </div>
+                                <p className="text-light-100 dark:text-gray-900 font-normal text-xl leading-5">
+                                    Thriller / Mystery
                                 </p>
+                                <div className="flex gap-1">
+                                    <Image
+                                        src={imagesAddresses.icons.greenBook}
+                                        alt="star"
+                                        width={16}
+                                        height={16}
+                                    />
+                                    <p className="text-light-100 dark:text-gray-900 font-normal text-base">
+                                        Borrowed on Dec 31
+                                    </p>
+                                </div>
+                                <div className="flex justify-between items-center">
+                                    <div className="flex gap-1">
+                                        <Image
+                                            src={imagesAddresses.icons.calender}
+                                            alt="calendar"
+                                            width={16}
+                                            height={16}
+                                        />
+                                        <p className="text-light-100 dark:text-gray-900 font-normal text-base">
+                                            04 days left to due
+                                        </p>
+                                    </div>
+                                    <Image
+                                        src={imagesAddresses.icons.list}
+                                        alt="list"
+                                        width={24}
+                                        height={24}
+                                    />
+                                </div>
                             </div>
-                            <Image
-                                src={imagesAddresses.icons.list}
-                                alt="list"
-                                width={24}
-                                height={24}
-                            />
-                        </div>
-                    </div>
-                ))}
+                        </SwiperSlide>
+                    ))}
+                </Swiper>
             </div>
+
             {/* Mobile */}
-            <div className="grid grid-cols-1 gap-4 sm:hidden py-2">
+            <div className="sm:hidden grid grid-cols-1 gap-4 py-2">
                 {books.slice(0, visibleCount).map((item) => (
                     <div
                         key={item}
-                        className="flex-shrink-0 w-full p-4  flex flex-col gap-3 bg-[url('/images/loginBg.png')] bg-gray-900 bg-cover dark:bg-gray-50 rounded-lg"
+                        className="flex-shrink-0 w-full p-4 flex flex-col gap-3 bg-[url('/images/loginBg.png')] bg-gray-900 bg-cover dark:bg-gray-50 rounded-lg"
                     >
                         <div className="bg-gold500/60 rounded-lg px-8 py-4 flex justify-center">
                             <Image
@@ -87,7 +107,6 @@ const BorrowedBooks = () => {
                                 alt="book"
                                 width={120}
                                 height={120}
-                                className="-ml-2 md:-ml-4"
                             />
                         </div>
                         <div className="flex flex-col gap-1">
@@ -134,48 +153,50 @@ const BorrowedBooks = () => {
                     </div>
                 ))}
             </div>
-            {visibleCount < books.length ? (
-                <div className="w-full flex sm:hidden">
-                    <CustomButton
-                        text="Show more"
-                        iconAddress={imagesAddresses.icons.arrowDown}
-                        iconPosition="left"
-                        color="yellow"
-                        containerClassName="w-full cursor-pointer dark:hidden block"
-                        onClick={() => setVisibleCount(books.length)}
-                    />
-                    <CustomButton
-                        text="Show more"
-                        iconAddress={imagesAddresses.icons.arrowDownWhite}
-                        iconPosition="left"
-                        color="yellow"
-                        containerClassName="w-full cursor-pointer dark:flex hidden"
-                        onClick={() => setVisibleCount(books.length)}
-                    />
-                </div>
-            ) : (
-                <div className="w-full flex sm:hidden">
-                    <CustomButton
-                        text="Show less"
-                        iconAddress={imagesAddresses.icons.arrowUp}
-                        iconPosition="left"
-                        color="yellow"
-                        containerClassName="w-full cursor-pointer dark:hidden block"
-                        onClick={() => setVisibleCount(INITIAL_COUNT)}
-                    />
-                    <CustomButton
-                        text="Show less"
-                        iconAddress={imagesAddresses.icons.arrowUpWhite}
-                        iconPosition="left"
-                        color="yellow"
-                        containerClassName="w-full cursor-pointer dark:flex hidden"
-                        onClick={() => setVisibleCount(INITIAL_COUNT)}
-                    />
-                </div>
-            )
-            }
+
+            <div className="w-full flex sm:hidden">
+                {visibleCount < books.length ? (
+                    <>
+                        <CustomButton
+                            text="Show more"
+                            iconAddress={imagesAddresses.icons.arrowDown}
+                            iconPosition="left"
+                            color="yellow"
+                            containerClassName="w-full cursor-pointer dark:hidden block"
+                            onClick={() => setVisibleCount(books.length)}
+                        />
+                        <CustomButton
+                            text="Show more"
+                            iconAddress={imagesAddresses.icons.arrowDownWhite}
+                            iconPosition="left"
+                            color="yellow"
+                            containerClassName="w-full cursor-pointer dark:flex hidden"
+                            onClick={() => setVisibleCount(books.length)}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <CustomButton
+                            text="Show less"
+                            iconAddress={imagesAddresses.icons.arrowUp}
+                            iconPosition="left"
+                            color="yellow"
+                            containerClassName="w-full cursor-pointer dark:hidden block"
+                            onClick={() => setVisibleCount(INITIAL_COUNT)}
+                        />
+                        <CustomButton
+                            text="Show less"
+                            iconAddress={imagesAddresses.icons.arrowUpWhite}
+                            iconPosition="left"
+                            color="yellow"
+                            containerClassName="w-full cursor-pointer dark:flex hidden"
+                            onClick={() => setVisibleCount(INITIAL_COUNT)}
+                        />
+                    </>
+                )}
+            </div>
         </div>
     );
-}
+};
 
 export default BorrowedBooks;
