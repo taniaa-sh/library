2a7f80ce24892f78d26b8d7ea737554aa48bec9a
tania shafiee <taniashafiee78@gmail.com>
2026-02-28 08:15:35 +0330

    fix footer ui

--- src/app/(user)/(root)/_components/Footer.tsx
+++ src/app/(user)/(root)/_components/Footer.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 const Footer = () => {
     return (
-        <footer className="bg-gradient-to-b from-gold100 to-gold200 dark:from-gold400 dark:to-gold500 text-black dark:text-white px-6 py-12 mt-16 rounded-t-4xl">
+        <footer className="bg-gradient-to-b from-gold100 to-gold200 dark:from-gold600 dark:to-gold700 text-black dark:text-white px-6 pt-12 pb-2 mt-16 rounded-t-4xl">
             <div className="max-w-7xl mx-auto flex flex-col md:flex-row justify-between items-start gap-8">
                 {/* Logo */}
                 <div className="flex flex-col items-start gap-4">
@@ -36,28 +36,27 @@ const Footer = () => {
                 </div>
                 <div className="flex flex-col gap-2">
                     <h3 className="font-semibold text-lg">Links</h3>
-                    <div className="flex flex-col md:flex-row md:gap-4">
+                    <div className="flex flex-col gap-2 md:flex-row md:gap-4">
                         <Link
                             href={SiteUrls.dashbord}
-                            className="hover:!text-gold400 dark:hover:!text-gold800 group relative transition-colors duration-200"
+                            className="hover:!text-gold400 dark:hover:!text-gold200 group relative transition-colors duration-200"
                         >
                             Dashboard
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold800 group-hover:w-full transition-all duration-300" />
+                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold200 group-hover:w-full transition-all duration-300" />
                         </Link>
                         <Link
                             href={SiteUrls.profile}
-                            className="hover:!text-gold400 dark:hover:!text-gold800 group relative transition-colors duration-200"
+                            className="hover:!text-gold400 dark:hover:!text-gold200 group relative transition-colors duration-200"
                         >
                             Profile
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold800 group-hover:w-full transition-all duration-300" />
+                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold200 group-hover:w-full transition-all duration-300" />
                         </Link>
-                        <Link
+                            <Link
                             href={SiteUrls.search}
-                            className="hover:!text-gold800 dark:hover:!text-gold400 group relative transition-colors duration-200"
+                            className="hover:!text-gold400 dark:hover:!text-gold200 group relative transition-colors duration-200"
                         >
                             books
-
-                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold800 group-hover:w-full transition-all duration-300" />
+                            <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-gold400 dark:bg-gold200 group-hover:w-full transition-all duration-300" />
                         </Link>
                     </div>
                 </div>
